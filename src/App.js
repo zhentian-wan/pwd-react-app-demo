3f8768c6ed7b3f879419013355c7711b9be54554
zhentian-wan <answer881215@gmail.com>
2018-12-31 15:34:53 +0200

    Camera for PWA

--- src/App.js
+++ src/App.js
@@ -1,28 +1,153 @@
+
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
 
-const ITEMS_URL = 'https://192.168.1.106:4567/items.json';
-class App extends Component {
+import GreyProfile from './grey_profile.png'
+import Back from './back.png'
+
+// const ITEMS_URL = "http://[YOUR LOCAL IP ADDRESS]:4567/items.json"
+const ITEMS_URL = "http://localhost:4567/items.json"
+
+class Profile extends Component {
+
+  state = {
+    image: null,
+    supportsCamera: 'mediaDevices' in navigator
+  }
+
+  changeImage = (e) => {
+    this.setState({
+      image: URL.createObjectURL(e.target.files[0])
+    })
+  }
+
+  startChangeImage = () => {
+    this.setState({ enableCamera: !this.state.enableCamera })
+  }
+
+  takeImage = () => {
+    this._canvas.width = this._video.videoWidth
+    this._canvas.height = this._video.videoHeight
+
+    this._canvas.getContext('2d').drawImage(
+      this._video,
+      0, 0,
+      this._video.videoWidth,
+      this._video.videoHeight
+    )
+
+    this._video.srcObject.getVideoTracks().forEach(track => {
+      track.stop()
+    })
+
+    this.setState({
+      image: this._canvas.toDataURL(),
+      enableCamera: false
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-light bg-light">
+            <span className="navbar-brand mb-0 h1">
+              <Link to="/">
+                <img src={Back} alt="logo" style={{ height: 30 }} />
+              </Link>
+              Profile
+            </span>
+        </nav>
+
+        <div style={{ textAlign: 'center' }}>
+          <img 
+            src={this.state.image || GreyProfile} alt="profile" 
+            style={{ height: 200, marginTop: 50 }} 
+          />
+          <p style={{ color: '#888', fontSize: 20 }}>username</p>
+
+          {
+            this.state.enableCamera &&
+            <div>
+              <video 
+                ref={c => {
+                  this._video = c
+                  if(this._video) {
+                    navigator.mediaDevices.getUserMedia({ video: true })
+                      .then(stream => this._video.srcObject = stream)
+                  }
+                }}
+                controls={false} autoPlay
+                style={{ width: '100%', maxWidth: 300 }}
+              ></video>
+
+              <br />
+
+              <button
+                onClick={this.takeImage}
+              >Take Image</button>
+
+              <canvas 
+                ref={c => this._canvas = c}
+                style={{ display: 'none' }}
+              />
+
+            </div>
+          }
+
+          <br />
+          {
+            this.state.supportsCamera &&
+            <button
+              onClick={this.startChangeImage}
+            >
+              Toggle Camera
+            </button>
+          }
+        </div>
+
+      </div>
+    )
+  }
+
+}
+
+
+class List extends Component {
 
   state = {
     items: [],
     loading: true,
-    todoItem: ''
+    todoItem: '',
+    offline: !navigator.onLine
   }
 
   componentDidMount() {
-    fetch(`${ITEMS_URL}/items.json`)
+    fetch(ITEMS_URL)
     .then(response => response.json())
     .then(items => {
       this.setState({ items, loading: false })
     })
+
+    window.addEventListener('online',  this.setOfflineStatus)
+    window.addEventListener('offline',  this.setOfflineStatus)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('online', this.setOfflineStatus)
+    window.removeEventListener('offline', this.setOfflineStatus)
+  }
+
+  setOfflineStatus = () => {
+    this.setState({ offline: !navigator.onLine })
   }
 
   addItem = (e) => {
     e.preventDefault()
 
-    fetch(`${ITEMS_URL}/items.json`, {
+    fetch(ITEMS_URL, {
       method: 'POST',
       body: JSON.stringify({ item: this.state.todoItem }),
       headers:{
@@ -34,7 +159,7 @@ class App extends Component {
       if(items.error) {
         alert(items.error)
       } else {
-        this.setState({ items })
+        this.setState({ items })        
       }
     })
 
@@ -42,7 +167,7 @@ class App extends Component {
   }
 
   deleteItem = (itemId) => {
-    fetch(`${ITEMS_URL}/items.json`, {
+    fetch(ITEMS_URL, {
       method: 'DELETE',
       body: JSON.stringify({ id: itemId }),
       headers:{
@@ -54,7 +179,7 @@ class App extends Component {
       if(items.error) {
         alert(items.error)
       } else {
-        this.setState({ items })
+        this.setState({ items })        
       }
     })
   }
@@ -64,8 +189,17 @@ class App extends Component {
       <div className="App">
         <nav className="navbar navbar-light bg-light">
           <span className="navbar-brand mb-0 h1">
-            <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} alt="logo" />
             Todo List
+          </span>
+          {
+            this.state.offline &&
+            <span className="badge badge-danger my-3">
+              Offline
+            </span>
+          }
+          <span>
+            <Link to="/profile">Profile</Link>
           </span>
         </nav>
 
@@ -133,4 +267,12 @@ class App extends Component {
   }
 }
 
-export default App;
+
+
+export default () =>
+  <Router>
+    <div>
+      <Route path="/" exact component={List} />
+      <Route path="/profile" exact component={Profile} />
+    </div>
+  </Router>
